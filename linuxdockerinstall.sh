c23c65f21228ee438b113bbd396226feb9e2843f
navalesagar <165131807+navalesagar@users.noreply.github.com>
2026-02-04 00:26:01 +0530

    Update linuxdockerinstall.sh

--- linuxdockerinstall.sh
+++ linuxdockerinstall.sh
@@ -1,6 +1,7 @@
-yum update -y
-yum install docker -y 
-service start docker 
+sudo yum update -y
+sudo yum install docker -y 
+sudo service docker start
+sudo usermod -a -G docker ec2-user
 docker --version
 docker info
 
